a70bcdec4a40fb27fcb0c9a785d3306c4c4d9fd6
ThiagoSzz <thiagosantosrs2@hotmail.com>
2024-11-17 16:45:20 -0300

    add etapa3 files

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
-/*
-Função principal para realização da análise sintática.
-
-Este arquivo será posterioremente substituído, não acrescente nada.
-*/
 #include <stdio.h>
-#include "parser.tab.h" //arquivo gerado com bison -d parser.y
+#include "ast.h"
+
+extern int yyparse(void);
 extern int yylex_destroy(void);
+
+void *arvore = NULL;
+void exporta(void *arvore);
+void free_ast(Node *node);
 
 int main(int argc, char **argv)
 {
   int ret = yyparse();
+  exporta(arvore);
+  free_ast(arvore);
+  arvore = NULL;
   yylex_destroy();
   return ret;
 }
